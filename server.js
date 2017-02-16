58f1d7fa9cd75104d3d4c9a242ac5df440f995eb
Jeremiah Gibson <jeremiah72654@Gmail.com>
2017-02-16 04:48:24 +0000

    Refactor config app use to another file * Removing secured.js add rules to server.js * Updated server.js to use auth0 jwt security

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 var express = require("express");
-var bodyParser = require("body-parser");
 var firebase = require('firebase-admin');
 var firebaseAdmin = require("firebase-admin");
 
@@ -22,29 +21,11 @@ app.adminApp = firebaseAdmin.initializeApp(credentials);
 app.adminDB = app.adminApp.database();
 app.adminMessaging = app.adminApp.messaging();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(function (req, res, next) {
 
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://jeremiahgibson.io');
+require("./src/config.js")(app);
 
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-}); 
 var routes = require("./src/routes.js")(app);
 
 var server = app.listen(process.env.NODE_PORT, function () {
     console.log("Listening on port %s...", server.address().port);
 });
-
