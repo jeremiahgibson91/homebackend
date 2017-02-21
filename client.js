cd943e4cb01d28b4315e07227d0d90a66bfc265d
Jeremiah Gibson <jgibson@tzmedical.com>
2017-02-20 18:46:45 -0800

    Refactoring client to promises

--- client.js
+++ client.js
@@ -1,39 +1,44 @@
 /* eslint-env node */
-var request = require("request");
+var rp = require("request-promise");
+var secrets = require('./secrets');
 
-var opts = {
-  method: 'POST',
-  url: 'https://homebase.auth0.com/oauth/token',
-  headers: {
-    'content-type': 'application/json'
-  },
-  body: '{"client_id":"MEQNKprc6kunb5fHWjYsmsGINQ1s2YpZ","client_secret":"3_fpzUHPtLIgzmzNnA2zWlVZPtLGy73ts1DqOUq7J4wm5OcPJv0zAWHd7z4LeVXY","audience":"https://api.jeremiahgibson.io","grant_type":"client_credentials"}'
-};
+function getClientToken() {
+  var options = {
+    method: 'POST',
+    url: 'https://homebase.auth0.com/oauth/token',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(secrets)
+  };
+  return rp(options);
+}
 
-var postBody = {
-  title: 'Hello',
-  body: 'World'
-};
-
-request(opts, function requestFunction(error, response, body) {
-  if (error) {
-    throw new Error(error);
-  }
-
+function sendFCM(message, token) {
   var options = {
     method: 'POST',
     uri: 'https://api.jeremiahgibson.io/message/send',
     headers: {
       contentType: 'application/json',
-      authorization: 'Bearer ' + JSON.parse(body).access_token
+      authorization: 'Bearer ' + token
     },
-    json: postBody
+    json: message
   };
+  return rp(options);
+}
 
-  request(options, function requestCallback(err, res, b) {
-    if (err) {
-      throw new Error(err);
-    }
-    console.log(body);
-  });
-});
+getClientToken()
+  .then((body) => {
+    var token = JSON.parse(body).access_token;
+    var message = {
+      title: 'Hello',
+      body: 'World'
+    };
+
+    sendFCM(message, token)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch(err => console.log(err));
+  })
+  .catch(err => console.log(err));
